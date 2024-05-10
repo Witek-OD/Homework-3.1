ea3083afb84999d75ee5fd0abe06c3019473deef
Wiktor Stoianov <stojanov87@gmail.com>
2024-05-10 09:39:17 +0300

    typeof to consol

---
+++ script.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', () => {
+
+    console.log(typeof 18);
+
+    console.log(typeof 'str');
+
+    console.log(typeof true);
+
+    console.log(typeof function() {});
+
+    console.log(typeof null);
+
+});
